de3a52e76fb5f5707fdeb34f0bf885fe90266200
cammykaz <119760109+cammykaz@users.noreply.github.com>
2024-10-07 17:03:30 -0700

    Arrange tasks by priority in table view

--- src/components/TableView.js
+++ src/components/TableView.js
@@ -5,9 +5,11 @@ import TaskRow from './TaskRow';
 import './TableView.css'; // Assuming you have specific styles for TableView
 
 function TableView({ tasks, onDelete, onUpdate }) {
+  const tasksSortedByPriority = tasks.slice().sort((a, b) => a.priority.localeCompare(b.priority));
+  
   return (
     <div className="taskGrid">
-      {tasks.map((task) => (
+      {tasksSortedByPriority.map((task) => (
         <TaskRow key={task.id} {...task} onDelete={onDelete} onUpdate={onUpdate} />
       ))}
     </div>
